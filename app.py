786c816f748e1a0097b1bca4d96468ccd95764b1
Jeff Peng <jeffpengyj@hotmail.com>
2021-04-28 22:53:47 -0400

    Api Routing

--- app.py
+++ app.py
@@ -1,87 +1,16 @@
-from flask import Flask, jsonify, request
-from http import HTTPStatus
+from flask import Flask
+from flask_restful import Api
+from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
 
 app = Flask(__name__)
+api = Api(app)
 
-recipes = [
-    {
-        'id': 1,
+api.add_resource(RecipeListResource, '/recipes')
 
-        'name': 'Egg Salad',
+api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
 
-        'description': 'This is a lovely egg salad recipe.'
-
-    },
-    {
-
-        'id': 2, 'name': 'Tomato Pasta',
-
-        'description': 'This is a lovely tomato pasta recipe.'
-
-    }
-]
-
-
-@app.route('/recipes', methods=['GET'])
-def get_recipes():
-    return jsonify({'data': recipes})
-
-
-@app.route('/recipes/<int:recipe_id>', methods=['GET'])
-def get_recipe(recipe_id):
-    recipe = next(
-        (item for item in recipes if item['id'] == recipe_id), None)
-
-    if recipe:
-        return jsonify(recipe)
-
-    return jsonify({'message': 'recipe not found'}), HTTPStatus.NOT_FOUND
-
-
-@app.route('/recipes', methods=['POST'])
-def create_recipe():
-    data = request.get_json()
-    name = data.get('name')
-    description = data.get('description')
-    recipe = {
-        'id': len(recipes) + 1,
-        'name': name,
-        'description': description
-    }
-    recipes.append(recipe)
-
-    return jsonify(recipe), HTTPStatus.CREATED
-
-
-@app.route('/recipes/<int:recipe_id>', methods=['PUT'])
-def update_recipe(recipe_id):
-    recipe = next(
-        (item for item in recipes if item['id'] == recipe_id), None)
-
-    if not recipe:
-        return jsonify({'message': 'recipe not found'}), HTTPStatus.NOT_FOUND
-
-    data = request.get_json()
-
-    recipe.update(
-        {
-            'name': data.get('name'),
-            'description': data.get('description')
-        }
-    )
-
-    return jsonify(recipe)
-
-
-@app.route('/recipes/<int:recipe_id>', methods=['DELETE'])
-def delete_recipe(recipe_id):
-    for i in range(len(recipes)):
-        if (recipes[i]['id'] == recipe_id):
-            recipes.pop(i)
-            return jsonify({'message': 'recipe {recipe_id} has been deleted'})
-
-    return jsonify({'message': 'recipe not found'}), HTTPStatus.NOT_FOUND
-
+api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
 
 if __name__ == '__main__':
-    app.run()
+
+    app.run(port=5000, debug=True)
